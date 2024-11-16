6f961a6402002c82fbffb1e702c4bf2769e075e3
Peikkohi <peik.kalliokoski@helsinki.fi>
2024-11-16 19:45:49 +0200

    muokkaa nettisivu tukemaan vain juurisivua, kommentoi muu

--- src/app.py
+++ src/app.py
@@ -1,39 +1,41 @@
-from flask import redirect, render_template, request, jsonify, flash
-from db_helper import reset_db
-from repositories.todo_repository import get_todos, create_todo, set_done
-from config import app, test_env
-from util import validate_todo
+from flask import render_template #, redirect, request, jsonify, flash
+#from db_helper import reset_db
+#from repositories.todo_repository import get_todos, create_todo, set_done
+from config import app #, test_env
+#from util import validate_todo
+
+counter = 0
 
 @app.route("/")
 def index():
-    todos = get_todos()
-    unfinished = len([todo for todo in todos if not todo.done])
-    return render_template("index.html", todos=todos, unfinished=unfinished) 
+    global counter
+    counter += 1
+    return render_template("index.html", counter=counter)
 
-@app.route("/new_todo")
-def new():
-    return render_template("new_todo.html")
-
-@app.route("/create_todo", methods=["POST"])
-def todo_creation():
-    content = request.form.get("content")
-
-    try:
-        validate_todo(content)
-        create_todo(content)
-        return redirect("/")
-    except Exception as error:
-        flash(str(error))
-        return  redirect("/new_todo")
-
-@app.route("/toggle_todo/<todo_id>", methods=["POST"])
-def toggle_todo(todo_id):
-    set_done(todo_id)
-    return redirect("/")
-
-# testausta varten oleva reitti
-if test_env:
-    @app.route("/reset_db")
-    def reset_database():
-        reset_db()
-        return jsonify({ 'message': "db reset" })
+#@app.route("/new_todo")
+#def new():
+#    return render_template("new_todo.html")
+#
+#@app.route("/create_todo", methods=["POST"])
+#def todo_creation():
+#    content = request.form.get("content")
+#
+#    try:
+#        validate_todo(content)
+#        create_todo(content)
+#        return redirect("/")
+#    except Exception as error:
+#        flash(str(error))
+#        return  redirect("/new_todo")
+#
+#@app.route("/toggle_todo/<todo_id>", methods=["POST"])
+#def toggle_todo(todo_id):
+#    set_done(todo_id)
+#    return redirect("/")
+#
+## testausta varten oleva reitti
+#if test_env:
+#    @app.route("/reset_db")
+#    def reset_database():
+#        reset_db()
+#        return jsonify({ 'message': "db reset" })
