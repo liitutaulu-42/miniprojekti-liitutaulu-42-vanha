8e23e71dbe1ab690bb915e2e3049fa680cd104b1
iw545 <leo.ihalainen@windowslive.com>
2024-11-19 12:46:18 +0200

    tietokantaan taulukon alustus placeholder tiedoilla

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,23 +1,23 @@
 from config import db, app
 from sqlalchemy import text
 
-table_name = "todos"
+table_name = "Artikkelit"
 
 
-def table_exists(name):
-    sql_table_existence = text(
-        "SELECT EXISTS ("
-        "  SELECT 1"
-        "  FROM information_schema.tables"
-        f" WHERE table_name = '{name}'"
-        ")"
-    )
+# def table_exists(name):
+#     sql_table_existence = text(
+#         "SELECT EXISTS ("
+#         "  SELECT 1"
+#         "  FROM information_schema.tables"
+#         f" WHERE table_name = '{name}'"
+#         ")"
+#     )
 
-    print(f"Checking if table {name} exists")
-    print(sql_table_existence)
+#     print(f"Checking if table {name} exists")
+#     print(sql_table_existence)
 
-    result = db.session.execute(sql_table_existence)
-    return result.fetchall()[0][0]
+#     result = db.session.execute(sql_table_existence)
+#     return result.fetchall()[0][0]
 
 
 def reset_db():
@@ -28,18 +28,21 @@ def reset_db():
 
 
 def setup_db():
-    if table_exists(table_name):
-        print(f"Table {table_name} exists, dropping")
-        sql = text(f"DROP TABLE {table_name}")
-        db.session.execute(sql)
-        db.session.commit()
+    # if table_exists(table_name):
+    #     print(f"Table {table_name} exists, dropping")
+    #     sql = text(f"DROP TABLE {table_name}")
+    #     db.session.execute(sql)
+    #     db.session.commit()
 
     print(f"Creating table {table_name}")
     sql = text(
         f"CREATE TABLE {table_name} ("
         "  id SERIAL PRIMARY KEY, "
-        "  content TEXT NOT NULL,"
-        "  done BOOLEAN DEFAULT FALSE"
+        "  key TEXT NOT NULL,"
+        "  author TEXT NOT NULL,"
+        "  title TEXT NOT NULL,"
+        "  journal TEXT NOT NULL,"
+        "  year INT NOT NULL"
         ")"
     )
 
